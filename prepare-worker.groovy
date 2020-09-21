ede28955d4c6ed03b0aa50a729683e2bd4e01e43
davron1989 <davron.n@yahoo.com>
2020-09-21 13:07:48 -0500

    added 'final'

--- prepare-worker.groovy
+++ prepare-worker.groovy
@@ -3,22 +3,22 @@ properties([
         string(defaultValue: '', description: 'Please enter VM IP address', name: 'nodeIP', trim: true)
         ])
     ])
+
 if (nodeIP?.trim()) {
     node {
         withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-master-ssh-key', keyFileVariable: 'SSHKEY', passphraseVariable: '', usernameVariable: 'SSHUSER')]) {
             stage('Init') {    
-                sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY  SSHUSER@${nodeIP} yum install epel-release -y'
+                sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY  $SSHUSER@${nodeIP} yum install epel-release -y'
             }
             stage('Instal Git') {
-                sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY  SSHUSER@${nodeIP} yum install git -y'            
+                sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY  $SSHUSER@${nodeIP} yum install git -y'            
             }
             stage('Install Java'){
-                sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY  SSHUSER@${nodeIP} yum install java-1.8.0-openjdk-devel -y'
+                sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY  $SSHUSER@${nodeIP} yum install java-1.8.0-openjdk-devel -y'
             }
         }
     }
 }
-
-else{
+else {
     error 'Please enter vald IP address'
 }

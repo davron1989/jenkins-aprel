345161ca22fd5b71498c5ce348e34bd94ec138f9
davron1989 <davron.n@yahoo.com>
2020-09-21 15:23:53 -0500

    added 'terraform'

--- prepare-worker.groovy
+++ prepare-worker.groovy
@@ -17,23 +17,23 @@ if (nodeIP?.trim()) {
                 sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY  $SSHUSER@${nodeIP} yum install java-1.8.0-openjdk-devel -y'
             }
             stage("Install Ansible"){
-                sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${nodeIP} yum install ansible -y'
+                sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSER@${nodeIP} yum install ansible -y'
             }
 
             stage("Install Terraform"){
-                sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${nodeIP} yum install -y wget unzip'
+                sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSER@${nodeIP} yum install -y wget unzip'
 
-                sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${nodeIP} wget https://releases.hashicorp.com/terraform/0.13.1/terraform_0.13.1_linux_amd64.zip'
-                sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${nodeIP} unzip terraform_0.13.1_linux_amd64.zip'
-                sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${nodeIP} mv terraform /usr/bin/terraform-0.13'
+                sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSER@${nodeIP} wget https://releases.hashicorp.com/terraform/0.13.1/terraform_0.13.1_linux_amd64.zip'
+                sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSER@${nodeIP} unzip terraform_0.13.1_linux_amd64.zip'
+                sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSER@${nodeIP} mv terraform /usr/bin/terraform-0.13'
 
-                sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${nodeIP} wget https://releases.hashicorp.com/terraform/0.12.29/terraform_0.12.29_linux_amd64.zip'
-                sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${nodeIP} unzip terraform_0.12.29_linux_amd64.zip'
-                sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${nodeIP} mv terraform /usr/bin/terraform-0.12'
+                sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSER@${nodeIP} wget https://releases.hashicorp.com/terraform/0.12.29/terraform_0.12.29_linux_amd64.zip'
+                sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSER@${nodeIP} unzip terraform_0.12.29_linux_amd64.zip'
+                sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSER@${nodeIP} mv terraform /usr/bin/terraform-0.12'
 
-                sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${nodeIP} wget https://releases.hashicorp.com/terraform/0.11.14/terraform_0.11.14_linux_amd64.zip'
-                sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${nodeIP} unzip terraform_0.11.14_linux_amd64.zip'
-                sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${nodeIP} mv terraform /usr/bin/terraform-0.11'
+                sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSER@${nodeIP} wget https://releases.hashicorp.com/terraform/0.11.14/terraform_0.11.14_linux_amd64.zip'
+                sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSER@${nodeIP} unzip terraform_0.11.14_linux_amd64.zip'
+                sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSER@${nodeIP} mv terraform /usr/bin/terraform-0.11'
             }
         }
     }

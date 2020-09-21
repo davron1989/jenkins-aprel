2bda0da5865ce053da2e579888dbf123872e9e9e
davron1989 <davron.n@yahoo.com>
2020-09-21 12:25:37 -0500

    added 'final'

--- prepare-worker.groovy
+++ prepare-worker.groovy
@@ -1,18 +1,24 @@
 properties([
     parameters([
-        string(defaultValue: '', description: 'Please enter VM IP address', name: 'Node-IP', trim: true)
+        string(defaultValue: '', description: 'Please enter VM IP address', name: 'nodeIP', trim: true)
         ])
     ])
-node {
-    withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-master-ssh-key', keyFileVariable: 'SSHKEY', passphraseVariable: '', usernameVariable: 'SSHUSER')]) {
-        stage('Init') {    
-            sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY  SSHUSER@${Node-IP} yum install epel-release -y'
-        }
-        stage('Instal Git') {
-            sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY  SSHUSER@${Node-IP} yum install git -y'            
-        }
-        stage('Install Java'){
-            sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY  SSHUSER@${Node-IP} yum install java-1.8.0-openjdk-devel -y'
+if (nodeIP?.trim()) {
+    node {
+        withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-master-ssh-key', keyFileVariable: 'SSHKEY', passphraseVariable: '', usernameVariable: 'SSHUSER')]) {
+            stage('Init') {    
+                sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY  SSHUSER@${nodeIP} yum install epel-release -y'
+            }
+            stage('Instal Git') {
+                sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY  SSHUSER@${nodeIP} yum install git -y'            
+            }
+            stage('Install Java'){
+                sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY  SSHUSER@${nodeIP} yum install java-1.8.0-openjdk-devel -y'
+            }
         }
     }
+}
+
+else{
+    error 'Please enter vald IP address'
 }

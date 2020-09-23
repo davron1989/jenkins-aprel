93303f68fecead6b3a49fce93de1b73007b3b904
davron1989 <davron.n@yahoo.com>
2020-09-23 14:51:30 -0500

    added 'final'

--- terraform-vpc.groovy
+++ terraform-vpc.groovy
@@ -1,45 +1,60 @@
 properties([
     parameters([
-        booleanParam(defaultValue: true, description: 'Do You want to apply?', name: 'terraform_apply '), 
-        booleanParam(defaultValue: true, description: 'Do You want to destroy?', name: 'terraform_destroy')
+        booleanParam(defaultValue: true, description: 'Do You want to apply?', name: 'terraform_apply'), 
+        booleanParam(defaultValue: true, description: 'Do You want to destroy?', name: 'terraform_destroy'),
+        choice(choices: ['dev', 'qa', 'prod'], description: 'In which environment do you want to run this script?', name: 'environment') 
     ])
 ])
 
+def aws_region_var = ''
+
+if (params.environment){
+    aws_region_var == "us-east-1"
+}
+else if (params.environment){
+    aws_region_var == "us-east-2"
+}
+else if (params.environment){
+    aws_region_var == "us-west-2"
+}
+
 node {
     stage("Pull Repo") {
+        cleanWs()
         git url: 'https://github.com/davron1989/terraform-vpc.git'
     }
     withCredentials([usernamePassword(credentialsId: 'jenkins-aws-access-key', passwordVariable: 'AWS_SECRET_ACCESS_KEY', usernameVariable: 'AWS_ACCESS_KEY_ID')]) {
-        stage("Terraform Init") {
-            sh '''
-                bash setenv.sh dev.tfvars
-                export AWS_REGION=us-east-1
-                terraform-0.13 init
-                terraform-0.13 plan -var-file dev.tfvars
-            '''
-        }
-        if (terraform_apply.toBoolean()){
-            stage("Terraform Apply") {
-            sh '''
-                export AWS_REGION=us-east-1
-                terraform-0.13 apply  -var-file dev.tfvars  -auto-approve
-            '''
+        withEnv(["AWS_REGION=${aws_region_var}"]){
+            
+            stage("Terraform Init") {
+                sh """
+                    bash setenv.sh ${environment}.tfvars
+                    terraform-0.13 init
+                """
+            }
+            if (terraform_apply.toBoolean()) {
+                stage("Terraform Apply") {
+                    sh """
+                        terraform-0.13 apply  -var-file ${environment}.tfvars  -auto-approve
+                    """
+                }
             }
 
-        }
-
-        else if (terraform_destroy.toBoolean()){
-            stage("Terraform Destroy") {
-            sh '''
-                export AWS_REGION=us-east-1
-                terraform-0.13 destroy  -var-file dev.tfvars  -auto-approve
-            '''
+            else if (terraform_destroy.toBoolean()){
+                stage("Terraform Destroy") {
+                    sh """
+                        terraform-0.13 destroy  -var-file ${environment}.tfvars  -auto-approve
+                    """
+                }
             }
-
+            else {
+                stage("Terraform Plan"){
+                    sh """
+                        export AWS_REGION=${aws_region_var}
+                        terraform-0.13 plan -var-file ${environment}.tfvars
+                    """
+                }
+            }
         }
-        else (Unknowen){
-            sh 'echo "Unknowen"'
-        }
-        
     }
 }

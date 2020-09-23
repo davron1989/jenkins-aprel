a2a78e0ad93b6bf11723896c2812b6cbe8007a12
davron1989 <davron.n@yahoo.com>
2020-09-22 21:05:43 -0500

    added "terraform apply"

---
+++ terraform-vpc.groovy
@@ -0,0 +1,45 @@
+properties([
+    parameters([
+        booleanParam(defaultValue: true, description: 'Do You want to apply?', name: 'terraform_apply '), 
+        booleanParam(defaultValue: true, description: 'Do You want to destroy?', name: 'terraform_destroy')
+    ])
+])
+
+node {
+    stage("Pull Repo") {
+        git branch: 'solution', url: 'https://github.com/davron1989/terraform-vpc.git'
+    }
+    withCredentials([usernamePassword(credentialsId: 'jenkins-aws-access-key', passwordVariable: 'AWS_SECRET_ACCESS_KEY', usernameVariable: 'AWS_ACCESS_KEY_ID')]) {
+        stage("Terraform Init") {
+            sh '''
+                bash setenv.sh dev.tfvars
+                export AWS_REGION=us-east-1
+                terraform-0.13 init
+                terraform-0.13 plan -var-file dev.tfvars
+            '''
+        }
+        if (terraform_apply.toBoolean()){
+            stage("Terraform Apply") {
+            sh '''
+                export AWS_REGION=us-east-1
+                terraform-0.13 apply  -var-file dev.tfvars  -auto-approve
+            '''
+            }
+
+        }
+
+        else if (terraform_destroy.toBoolean()){
+            stage("Terraform Destroy") {
+            sh '''
+                export AWS_REGION=us-east-1
+                terraform-0.13 destroy  -var-file dev.tfvars  -auto-approve
+            '''
+            }
+
+        }
+        else (Unknowen){
+            sh 'echo "Unknowen"'
+        }
+        
+    }
+}

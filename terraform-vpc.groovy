a71ddcc409416bfad1c877f8e50cda15ac2b561e
davron1989 <davron.n@yahoo.com>
2020-09-23 15:20:31 -0500

    added final

--- terraform-vpc.groovy
+++ terraform-vpc.groovy
@@ -23,15 +23,15 @@ node {
         cleanWs()
         git url: 'https://github.com/davron1989/terraform-vpc.git'
     }
+
     withCredentials([usernamePassword(credentialsId: 'jenkins-aws-access-key', passwordVariable: 'AWS_SECRET_ACCESS_KEY', usernameVariable: 'AWS_ACCESS_KEY_ID')]) {
-        withEnv(["AWS_REGION=${aws_region_var}"]){
-            
-            stage("Terraform Init") {
+        withEnv(["AWS_REGION=${aws_region_var}"]) {
+            stage("Terrraform Init"){
                 sh """
                     bash setenv.sh ${environment}.tfvars
                     terraform-0.13 init
                 """
-            }
+            } 
             if (terraform_apply.toBoolean()) {
                 stage("Terraform Apply") {
                     sh """

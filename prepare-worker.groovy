2f983648b042fa7f6ddf21494266f70803328874
davron1989 <davron.n@yahoo.com>
2020-09-25 11:40:37 -0500

    added intall packer

--- prepare-worker.groovy
+++ prepare-worker.groovy
@@ -35,6 +35,12 @@ if (nodeIP?.trim()) {
                 sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSER@${nodeIP} unzip terraform_0.11.14_linux_amd64.zip'
                 sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSER@${nodeIP} mv terraform /usr/bin/terraform-0.11'
             }
+            stage("Install Packer"){
+                sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${nodeIP} yum install -y yum-utils'
+                sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${nodeIP} yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo'
+                sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${nodeIP} mv /usr/sbin/packer /usr/sbin/packer_original'
+                sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${nodeIP} yum install -y packer'
+            }
         }
     }
 }
